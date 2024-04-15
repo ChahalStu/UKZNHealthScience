29495f5f544ac2e969f44f54738b95dea1059fb0
ChahalStu <218027455@stu.ukzn.ac.za>
2024-04-15 21:47:39 +0200

    removing main solution

--- MedicalSystemWebApp/MedicalSystemWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MedicalSystemWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
