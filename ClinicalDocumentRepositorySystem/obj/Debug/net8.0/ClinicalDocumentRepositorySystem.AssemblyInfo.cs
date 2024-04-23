74150a16241ea122da682c8f666f974984bb5ef9
ChahalStu <218027455@stu.ukzn.ac.za>
2024-04-23 08:31:08 +0200

    AddedPatientDetails

--- ClinicalDocumentRepositorySystem/obj/Debug/net8.0/ClinicalDocumentRepositorySystem.AssemblyInfo.cs
+++ ClinicalDocumentRepositorySystem/obj/Debug/net8.0/ClinicalDocumentRepositorySystem.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicalDocumentRepositorySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ac3a381b74fa7a2e939be8f5a9afb9a7575216")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d85d100f411ee07631d96d08f255b10a62a241")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicalDocumentRepositorySystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicalDocumentRepositorySystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
